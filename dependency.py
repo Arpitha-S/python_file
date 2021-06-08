8a80f1580237313e17462f76d31182bbc5aa8ebd
Arpitha-S <83060748+Arpitha-S@users.noreply.github.com>
2021-06-08 18:20:28 +0530

    Add files via upload

---
+++ dependency.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from datetime import datetime
+
+url = 'http://localhost:8080/api/v2/import-scan/'
+
+headers = {'Authorization': 'Token d7163c979de993065820c094150109a22c0e916f'}
+
+files = {'file': open('C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/defectdojo/dependency-check-report.xml','rb')}
+
+dia = datetime.today().strftime('%Y-%m-%d')
+print(dia)
+
+body = {'scan_date': dia,
+'scan_type': 'Dependency Check Scan',
+'engagement': '1',
+'lead': '1'
+}
+
+r = requests.post(url, headers=headers, files=files, verify=False, data=body)
+
+print("Status Code :%s"%r.status_code)
+print("Respuesta :%s"%r.text)
